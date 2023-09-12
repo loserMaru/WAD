96ede6e31f4d3ea0077d741e8140c55c53d43ae0
Marlen Osmanov <osmanov.m.i.20@gmail.com>
2023-09-12 15:49:36 +0300

    Lab 1 methods overriding

--- main.py
+++ main.py
@@ -8,57 +8,60 @@ class Product:
         self.shelf_life = shelf_life
         self.quantity = quantity
 
-    # Геттеры и сеттеры для Tun (наименование товара)
-    def getTun(self):
+    def __getattr__(self, name):
         return self.name
 
-    def setTun(self, name):
-        self.name = name
+    def __setattr__(self, name, value):
+        if name == 'name':
+            self.__dict__[name] = value
+        else:
+            super().__setattr__(name, value)
 
-    def toString(self):
+    def __str__(self):
         return f"ID: {self.id}, Наименование: {self.name}, UPC: {self.upc}, Производитель: {self.manufacturer}, Цена: {self.price}, Срок хранения: {self.shelf_life}, Количество: {self.quantity}"
 
-    def hashCode(self):
-        return hash(self.id)
+    def __hash__(self):
+        return hash(self.name)
 
 
-# Создаем массив объектов Product
 products = [
     Product(1, "Товар1", "123456", "Производитель1", 10.99, 365, 100),
     Product(2, "Товар2", "789012", "Производитель2", 15.49, 180, 50),
     Product(3, "Товар3", "345678", "Производитель1", 5.99, 90, 200),
     Product(4, "Товар4", "901234", "Производитель3", 8.99, 270, 75),
-]
+    Product(5, "Товар5", "723492", "Производитель2", 11.49, 250, 125)]
 
 
-# a) Список товаров для заданного наименования
 def find_products_by_name(products, name):
-    return [product for product in products if product.getTun() == name]
+    return [product for product in products if product.__getattr__('name') == name]
 
 
-# b) Список товаров для заданного наименования, цена которых не превосходит заданную
+name = "Товар1"  # Замените на нужное наименование
+matching_products = find_products_by_name(products, name)
+
+for product in matching_products:
+    print(product, '\n')
+
+
 def find_products_by_name_and_price(products, name, max_price):
-    return [product for product in products if product.getTun() == name and product.price <= max_price]
+    return [product for product in products if
+            product.__getattr__('name') == name and product.price <= max_price]
 
 
-# c) Список товаров, срок хранения которых больше заданного
+name = "Товар2"
+max_price = 21.0
+matching_products = find_products_by_name_and_price(products, name, max_price)
+
+for product in matching_products:
+    print(product, '\n')
+
+
 def find_products_by_shelf_life(products, min_shelf_life):
     return [product for product in products if product.shelf_life > min_shelf_life]
 
 
-# Примеры использования
-name_to_find = "Товар1"
-max_price_to_find = 11.0
-min_shelf_life_to_find = 200
+min_shelf_life = 200
+matching_products = find_products_by_shelf_life(products, min_shelf_life)
 
-print("a) Список товаров для заданного наименования:")
-for product in find_products_by_name(products, name_to_find):
-    print(product.toString())
-
-print("\nb) Список товаров для заданного наименования, цена которых не превосходит заданную:")
-for product in find_products_by_name_and_price(products, name_to_find, max_price_to_find):
-    print(product.toString())
-
-print("\nc) Список товаров, срок хранения которых больше заданного:")
-for product in find_products_by_shelf_life(products, min_shelf_life_to_find):
-    print(product.toString())
+for product in matching_products:
+    print(product)
